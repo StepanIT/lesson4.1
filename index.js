be7c40d3c1b6d71c73d961ab27035a836b635ca6
Степан Рудаков <19stepan94@mail.ru>
2024-06-26 23:24:17 +0500

    делаю задание 4.2

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
   'use strict'
   
+{
   const productTitle = 'Куртка';
   const productQuantity = '3';
   const productCategory = 'Одежда';
   const productPrice = '6000';
   const totalPrice = productPrice * productQuantity;
   
-  console.log('productTitle ' , productTitle);
-  console.log('totalPrice ' , totalPrice);
+  console.log('productTitle: ' , productTitle);
+  console.log('totalPrice: ' , totalPrice);
+} 
+{
+  const productTitle = prompt('productTitle');
+  const productQuantity = prompt('productQuantity');
+  const productCategory = prompt('productCategory');
+  const productPrice = prompt('productPrice');
+
+  console.log('productTitle: ' , productTitle);
+  console.log('productQuantity: ' , productQuantity);
+  console.log('productCategory: ' , productCategory);
+  console.log('productPrice: ' , productPrice);
+
+  console.log(typeof productTitle);
+  console.log(typeof productQuantity);
+  console.log(typeof productCategory);
+  console.log(typeof productPrice);
+}
