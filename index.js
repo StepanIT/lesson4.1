2c4469bafc69188cd11ec953dae74a5dedda6faf
Степан Рудаков <19stepan94@mail.ru>
2024-06-26 19:14:03 +0500

    сделал задание 4.1 и необязательное задание

---
+++ index.js
@@ -0,0 +1,10 @@
+  'use strict'
+  
+  const productTitle = 'Куртка';
+  const productQuantity = '3';
+  const productCategory = 'Одежда';
+  const productPrice = '6000';
+  const totalPrice = productPrice * productQuantity;
+  
+  console.log('productTitle ' , productTitle);
+  console.log('totalPrice ' , totalPrice);
