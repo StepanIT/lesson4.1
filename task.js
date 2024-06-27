86ebf1671445a82207b2fb06561672d79617da31
Степан Рудаков <19stepan94@mail.ru>
2024-06-27 17:15:37 +0500

    сделал третью задачу 4.3

--- task.js
+++ task.js
@@ -1,5 +1,6 @@
   'use strict'
 
+{
   const rain = Math.random();
   
   // console.log('rain:' , Math.round(rain));
@@ -11,3 +12,17 @@
   if (Math.round(rain) === 0) {
     console.log('Дождя нет!')
   }
+}
+  
+{
+  const numberOfPointsInMathematics = parseInt(prompt('Введите кол-во баллов по математике:'));
+  const numberOfPointsInRussianLanguage = parseInt(prompt('Введите кол-во баллов по русскому языку:'));
+  const numberOfPointsInComputerScience = parseInt(prompt('Введите кол-во баллов по информатике:'));
+  const totalPoints = numberOfPointsInMathematics + numberOfPointsInRussianLanguage + numberOfPointsInComputerScience;
+
+  if (totalPoints >= 265) {
+    console.log('Поздравляю, вы поступили на бюджет!');
+  } else {
+    console.log('Увы, вам не хватило баллов')
+  }
+}
