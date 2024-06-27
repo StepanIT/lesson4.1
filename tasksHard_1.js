099588175837b425a7053caabb4b53f74fa51fb0
Степан Рудаков <19stepan94@mail.ru>
2024-06-27 17:49:25 +0500

    сделал первую задачу дополнительного задания 4.3

--- tasksHard_1.js
+++ tasksHard_1.js
@@ -6,6 +6,18 @@
  const highIncome = userIncomeAmount * 0.30;
 
 
- if (userIncomeAmount < 15000 && userIncomeAmount > 0) {
-  console.log('Налог ' smallIncome);
+ if (userIncomeAmount <= 15000 && userIncomeAmount > 0) {
+  console.log('Налог ' , smallIncome);
+ }
+
+ if (userIncomeAmount >= 15000 && userIncomeAmount <= 50000) {
+  console.log('Налог ' , averageIncome);
+ }
+
+ if (userIncomeAmount > 50000) {
+  console.log('Налог ' , highIncome);
+ }
+
+ if (userIncomeAmount <= 0) {
+  console.log('Введите сумму больше 0');
  }
