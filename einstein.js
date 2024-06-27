e1ce6671b271308de1fb9fca1481767020555bdb
Степан Рудаков <19stepan94@mail.ru>
2024-06-27 16:28:31 +0500

    сделал первую задачу в 4.3

--- einstein.js
+++ einstein.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const myWeight = 87;
-const speedLight = 3e8;
-const bodyEnergy = myWeight * (speedLight ** 2);
+// const myWeight = 87;
+// const speedLight = 3e8;
+// const bodyEnergy = myWeight * (speedLight ** 2);
 
-console.log(bodyEnergy);
+// console.log(bodyEnergy);
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
   'use strict'
   
-{
-  const productTitle = 'Куртка';
-  const productQuantity = '3';
-  const productCategory = 'Одежда';
-  const productPrice = '6000';
-  const totalPrice = productPrice * productQuantity;
+// {
+//   const productTitle = 'Куртка';
+//   const productQuantity = '3';
+//   const productCategory = 'Одежда';
+//   const productPrice = '6000';
+//   const totalPrice = productPrice * productQuantity;
   
-  console.log('productTitle: ' , productTitle);
-  console.log('totalPrice: ' , totalPrice);
-}
+//   console.log('productTitle: ' , productTitle);
+//   console.log('totalPrice: ' , totalPrice);
+// }
 
 {
   const productTitle = prompt('Наименование товара');
@@ -18,15 +18,19 @@
   const productPrice = prompt('Цена товара');
   const totalPrice = productPrice * productQuantity;
 
-  console.log('Наименование товара: ' , productTitle);
-  console.log('Количество товара: ' , +productQuantity);
-  console.log('Категория товара: ' ,  productCategory);
-  console.log('Цена товара: ' ,  +productPrice);
-
-  console.log(typeof productTitle);
-  console.log(typeof +productQuantity);
-  console.log(typeof productCategory);
-  console.log(typeof +productPrice);
-
-  console.log(`"На складе ${productQuantity} единицы товара "${productTitle}" на сумму ${totalPrice} деревянных"`);
+  if (productQuantity >= 0 && productPrice >= 0) {
+    console.log('Наименование товара: ' , productTitle);
+    console.log('Количество товара: ' , +productQuantity);
+    console.log('Категория товара: ' ,  productCategory);
+    console.log('Цена товара: ' ,  +productPrice);
+  
+    console.log(typeof productTitle);
+    console.log(typeof +productQuantity);
+    console.log(typeof productCategory);
+    console.log(typeof +productPrice);
+  
+    console.log(`"На складе ${productQuantity} единицы товара "${productTitle}" на сумму ${totalPrice} деревянных"`);
+  } else {
+    console.log('Вы ввели некорректные данные');
+  }
 }
