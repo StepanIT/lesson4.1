258a5959c85b70b6feceeec60f1068d2b7f935cb
Степан Рудаков <19stepan94@mail.ru>
2024-06-27 18:10:04 +0500

    сделал вторую задачу в дополнительно задании 4.3

--- tasksHard_1.js
+++ tasksHard_1.js
@@ -5,7 +5,6 @@
  const averageIncome = userIncomeAmount * 0.20;
  const highIncome = userIncomeAmount * 0.30;
 
-
  if (userIncomeAmount <= 15000 && userIncomeAmount > 0) {
   console.log('Налог ' , smallIncome);
  }
