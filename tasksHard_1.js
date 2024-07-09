7fa6efb70be35ce34220bb10696a127c0ff1d67e
Степан <19stepan94@mail.ru>
2024-07-09 15:48:53 +0500

    переделал усложненные задачи в 4.3

--- tasksHard_1.js
+++ tasksHard_1.js
@@ -1,24 +1,18 @@
-//  'use strict'
+'use strict'
 
-// {
-//   const userIncomeAmount = parseInt(prompt('Введите ваш доход'));
-//   const smallIncome = userIncomeAmount * 0.13;
-//   const averageIncome = userIncomeAmount * 0.20;
-//   const highIncome = userIncomeAmount * 0.30;
+{
+  const userIncomeAmount = parseInt(prompt('Введите ваш доход'));
+  const smallIncome = userIncomeAmount * 0.13;
+  const averageIncome = userIncomeAmount * 0.20;
+  const highIncome = userIncomeAmount * 0.30;
 
-//   if (userIncomeAmount <= 15000 && userIncomeAmount > 0) {
-//     console.log('Налог ' , smallIncome);
-//   }
-
-//   if (userIncomeAmount >= 15000 && userIncomeAmount <= 50000) {
-//     console.log('Налог ' , averageIncome);
-//   }
-
-//   if (userIncomeAmount > 50000) {
-//     console.log('Налог ' , highIncome);
-//   }
-
-//   if (userIncomeAmount <= 0) {
-//     console.log('Введите сумму больше 0');
-//   }
-// }
+  if (userIncomeAmount > 50000) {
+    console.log('Налог ' , highIncome);
+  } else if (userIncomeAmount >= 15000) {
+    console.log('Налог ' , averageIncome);
+  } else if (userIncomeAmount >= 0) {
+    console.log('Налог ' , smallIncome);
+  } else {
+    console.log('Введите корректное значение');
+  };
+}
